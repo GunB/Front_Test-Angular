b05a45fb11d27733859cd1feacd630b033cea88c
Heiner Angarita <heinerangarita@gmail.com>
2016-03-04 23:00:14 -0500

    INIT

---
+++ app/config.rb
@@ -0,0 +1,23 @@
+# Set this to the root of your project when deployed:
+http_path = ""
+
+# Set the images directory relative to your http_path or change
+# the location of the images themselves using http_images_path:
+# http_images_dir = "assets/images"
+
+# Production Assets URL
+# http_images_path = "http://your-url-goes-here/img"
+
+css_dir = "/"
+sass_dir = "scss"
+images_dir = "assets/images/"
+javascripts_dir = "js"
+
+output_style = :compressed
+environment = :development
+
+relative_assets = true
+
+line_comments = false
+color_output = false
+Sass::Script::Number.precision = 10
